5039988d9ed4d72b6c8ee6feaae46a08256b7988
unknown <aivaras.goncarovas@gmail.com>
2020-05-20 18:21:10 +0300

    deleted:    db/mongo.js

--- db/mongo.js
+++
@@ -1,9 +0,0 @@
-const MongoClient = require('mongodb').MongoClient;
-const connectionString =
-  'mongodb+srv://dummy:ngSckQNcQlcKYrvB@cluster0-koonn.mongodb.net/test?retryWrites=true&w=majority';
-MongoClient.connect(connectionString, { useUnifiedTopology: true })
-  .then((client) => {
-    console.log('Connected to Database');
-    const db = client.db('very-cool-db');
-  })
-  .catch((error) => console.error(error));
